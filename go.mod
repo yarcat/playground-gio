7369521e4a18879072c8a88dc1c0d8a1dba33b40
Jaroslavs Samcuks <yarcat@gmail.com>
2020-09-20 10:55:23 +0200

    Rotate images using x/image
    This is an experiment that we will use in the future to generate GIFs.

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	gioui.org v0.0.0-20200912153211-2f67feafc0a2
 	golang.org/x/exp v0.0.0-20200908183739-ae8ad444f925 // indirect
-	golang.org/x/image v0.0.0-20200801110659-972c09e46d76 // indirect
+	golang.org/x/image v0.0.0-20200801110659-972c09e46d76
 	golang.org/x/sys v0.0.0-20200915084602-288bc346aa39 // indirect
 	golang.org/x/text v0.3.3 // indirect
 )
--- transition-app/main.go
+++ transition-app/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"log"
+	"math"
 	"os"
+
+	"golang.org/x/image/math/f64"
+
+	"golang.org/x/image/draw"
 
 	"gioui.org/app"
 	"github.com/yarcat/playground-gio/transition-app/res"
@@ -11,8 +18,8 @@ import (
 func main() {
 	go func() {
 		app := newTransitionApp(
-			mustDecodePNG(res.GopherSimplePNG),
-			mustDecodePNG(res.GopherPNG),
+			rotate(mustDecodePNG(res.GopherSimplePNG), math.Pi/6),
+			rotate(mustDecodePNG(res.GopherPNG), -math.Pi/6),
 		)
 		if err := app.mainloop(); err != nil {
 			log.Fatal("mainloop failed:", err)
@@ -20,4 +27,25 @@ func main() {
 		os.Exit(0)
 	}()
 	app.Main()
+}
+
+func rotate(src image.Image, r float64) image.Image {
+	sw, sh := float64(src.Bounds().Dx()), float64(src.Bounds().Dy())
+	dst := image.NewRGBA(image.Rectangle{
+		Max: image.Pt(int(sw*1.45), int(sh*1.45)),
+	})
+	// draw.Copy(dst, image.Point{}, src, dst.Bounds(), draw.Src, nil)
+	c, s := math.Cos(r), math.Sin(r)
+	x0, y0 := sw/2, sh/2
+	dw, dh := float64(dst.Bounds().Dx()), float64(dst.Bounds().Dy())
+	fmt.Println(sw, sh)
+	fmt.Println(dw, dh)
+	dx, dy := (dw-sw)/2, (dh-sh)/2
+	fmt.Println(dx, dy)
+	m := f64.Aff3{
+		c, -s, (-x0)*c - (-y0)*s + x0 + dx,
+		s, c, (-x0)*s + (-y0)*c + y0 + dy,
+	}
+	draw.NearestNeighbor.Transform(dst, m, src, dst.Rect, draw.Src, nil)
+	return dst
 }
