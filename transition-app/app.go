22882ff12627603a7aad6e5fcc3ddf8b8d182f5b
Jaroslavs Samcuks <yarcat@gmail.com>
2020-09-21 17:16:43 +0200

    redesigned ui

--- transition-app/app.go
+++ transition-app/app.go
@@ -3,68 +3,80 @@ package main
 import (
 	"image"
 	"image/color"
-	"math"
-
-	ycwidget "github.com/yarcat/playground-gio/transition-app/widget"
 
 	"gioui.org/app"
+	"gioui.org/f32"
 	"gioui.org/font/gofont"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	ycwidget "github.com/yarcat/playground-gio/transition-app/widget"
 )
 
 type transitionApp struct {
-	images      []*ycwidget.Image
-	states      []*ycwidget.AffineState
-	angles      []widget.Float
-	lastChanged *widget.Float
-	win         *app.Window
-	theme       *material.Theme
-	thumbnails  layout.List
-	// Real rotation is shiftedRotation - Pi
-	shiftedRotation widget.Float
+	win           *app.Window
+	theme         *material.Theme
+	thumbnails    layout.List
+	thumbnailImgs []*ycwidget.Image
 }
 
 func newTransitionApp(imgs ...image.Image) *transitionApp {
-	a := &transitionApp{
-		win:             app.NewWindow(),
-		theme:           material.NewTheme(gofont.Collection()),
-		shiftedRotation: widget.Float{Value: math.Pi},
-		thumbnails:      layout.List{Axis: layout.Vertical},
+	thumbnails := make([]*ycwidget.Image, 0, len(imgs))
+	for _, img := range imgs {
+		thumbnails = append(thumbnails, ycwidget.NewImage(img))
 	}
-	a.images = make([]*ycwidget.Image, len(imgs))
-	a.states = make([]*ycwidget.AffineState, len(imgs))
-	a.angles = make([]widget.Float, len(imgs))
-	for i, img := range imgs {
-		imgWidget := ycwidget.NewImage(img)
-		a.images[i] = imgWidget
-		w := ycwidget.DragAndRotate(imgWidget.Layout, &a.angles[i])
-		a.states[i] = &w
+	return &transitionApp{
+		win:           app.NewWindow(),
+		theme:         material.NewTheme(gofont.Collection()),
+		thumbnailImgs: thumbnails,
 	}
-	return a
 }
 
 func (a *transitionApp) mainloop() error {
 	ops := &op.Ops{}
 
+	thumbs := make(thumbnails, 0, len(a.thumbnailImgs))
+	for _, img := range a.thumbnailImgs {
+		thumbs = append(thumbs, &clickable{widget: img.Layout})
+	}
+
+	selected := 0
 	for e := range a.win.Events() {
 		switch e := e.(type) {
 		case system.FrameEvent:
 			gtx := layout.NewContext(ops, e)
 
-			for i, img := range a.states {
-				if img.Changed() {
-					a.lastChanged = &a.angles[i]
+			for i, c := range thumbs {
+				if c.button.Clicked() {
+					selected = i
 				}
-				img.Layout(gtx)
 			}
-
-			a.layoutRotationSlider(gtx)
-			a.layoutThumbnails(gtx)
+			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					axis := layout.Horizontal
+					if gtx.Constraints.Max.X < gtx.Constraints.Max.Y {
+						axis = layout.Vertical
+					}
+					return layout.Flex{Axis: axis}.Layout(gtx,
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return layout.Center.Layout(gtx, a.thumbnailImgs[selected].Layout)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							l := material.H5(a.theme, "PREVIEW\nPLACEHOLDER")
+							return layout.Center.Layout(gtx, l.Layout)
+						}),
+					)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return layout.S.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+						return thumbs.Layout(gtx, &a.thumbnails, selected)
+					})
+				}),
+			)
 
 			e.Frame(gtx.Ops)
 		case system.DestroyEvent:
@@ -74,39 +86,51 @@ func (a *transitionApp) mainloop() error {
 	return nil
 }
 
-func (a *transitionApp) layoutRotationSlider(gtx layout.Context) layout.Dimensions {
-	if a.lastChanged == nil {
-		return layout.Dimensions{Size: gtx.Constraints.Min}
-	}
-	return layout.S.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-		return layout.Flex{
-			Axis: layout.Horizontal,
-		}.Layout(gtx,
-			layout.Flexed(1, material.Slider(
-				a.theme,
-				a.lastChanged,
-				0,
-				2*math.Pi,
-			).Layout),
-		)
+const (
+	thumbnailInsetDp = 10
+	thumbnailSizePx  = 150
+)
+
+type thumbnails []*clickable
+
+func (th thumbnails) Layout(gtx layout.Context, list *layout.List, selected int) layout.Dimensions {
+	return list.Layout(gtx, len(th), func(gtx layout.Context, index int) layout.Dimensions {
+		return layout.UniformInset(unit.Dp(thumbnailInsetDp)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			return th[index].Layout(gtx, selected == index)
+		})
 	})
 }
 
-const thumbnailSize = 150
+type clickable struct {
+	widget layout.Widget
+	button widget.Clickable
+}
 
-var thumbnailInset = unit.Dp(2)
-
-func (a *transitionApp) layoutThumbnails(gtx layout.Context) layout.Dimensions {
-	return layout.E.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-		return a.thumbnails.Layout(gtx, len(a.images), func(gtx layout.Context, index int) layout.Dimensions {
-			return layout.UniformInset(thumbnailInset).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-				gtx.Constraints.Max = image.Pt(thumbnailSize, thumbnailSize)
-				return widget.Border{
-					Color:        color.RGBA{A: 0xff, R: 0x1f, G: 0x1f, B: 0x1f},
-					Width:        unit.Dp(1),
-					CornerRadius: unit.Dp(10),
-				}.Layout(gtx, a.images[index].Layout)
-			})
-		})
+func (btn *clickable) Layout(gtx layout.Context, selected bool) layout.Dimensions {
+	macro := op.Record(gtx.Ops)
+	cornerRad := unit.Dp(10)
+	d := material.Clickable(gtx, &btn.button, func(gtx layout.Context) layout.Dimensions {
+		gtx.Constraints.Max = image.Pt(thumbnailSizePx, thumbnailSizePx)
+		if !selected {
+			return btn.widget(gtx)
+		}
+		return widget.Border{
+			Color:        color.RGBA{A: 0xff, R: 0x1f, G: 0x1f, B: 0x1f},
+			Width:        unit.Dp(1),
+			CornerRadius: cornerRad,
+		}.Layout(gtx, btn.widget)
 	})
+	call := macro.Stop()
+
+	defer op.Push(gtx.Ops).Pop()
+	clip.RRect{
+		Rect: f32.Rectangle{Max: layout.FPt(d.Size)},
+		NW:   cornerRad.V * gtx.Metric.PxPerDp,
+		NE:   cornerRad.V * gtx.Metric.PxPerDp,
+		SW:   cornerRad.V * gtx.Metric.PxPerDp,
+		SE:   cornerRad.V * gtx.Metric.PxPerDp,
+	}.Add(gtx.Ops)
+	call.Add(gtx.Ops)
+
+	return d
 }
